82ccf50c749625cdbbd6a76f0e72c572bea4565c
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-27 00:30:29 +0330

    Multiplication tabel for a number

---
+++ HackerRank_03/JavaLoops.java
@@ -0,0 +1,15 @@
+package HackerRank_03;
+
+public class JavaLoops {
+    public static void main(String[] args){
+        
+        int n  = 2; 
+        for(int i = 1; i <= 10; i++){
+       
+          
+            System.out.println("2" + " x " + i + " => " + (n * i));
+        
+        }
+        
+    }
+}
