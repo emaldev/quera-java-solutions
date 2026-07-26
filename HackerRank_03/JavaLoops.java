91f986bcb3e8a8c5dfccb5564bde687a540c5ae4
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-27 01:08:50 +0330

    Sepuence of numbers

--- HackerRank_03/JavaLoops.java
+++ HackerRank_03/JavaLoops.java
@@ -1,14 +1,23 @@
 package HackerRank_03;
 
+import java.util.Scanner;
+
 public class JavaLoops {
     public static void main(String[] args){
-        
-        int n  = 2; 
-        for(int i = 1; i <= 10; i++){
-       
+        Scanner input = new Scanner(System.in);
+
+        int t = input.nextInt();
+        for(int i = 0; i < t; i++){       
+          int a = input.nextInt();
+          int b = input.nextInt();
+          int n = input.nextInt();
+          int sum = a;
+          for(int j = 0; j < n; j++){
+            sum += (int)(b * Math.pow(2,j));
+             System.out.println(sum + " ");
+             System.out.println();
+          }
           
-            System.out.println("2" + " x " + i + " => " + (n * i));
-        
         }
         
     }
