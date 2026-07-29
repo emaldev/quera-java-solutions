c2fc57412cfedc6a38217e0174a48157984a44ff
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-30 00:32:54 +0330

    Adding a dog class and this class extends the animal class

---
+++ DonA.java
@@ -0,0 +1,10 @@
+public class DonA extends AnimalManagement {
+
+    @Override
+    public void makeSound(){
+        super.makeSound();
+        System.out.println("Dog barks");
+    }
+    
+    
+}
