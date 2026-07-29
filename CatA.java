2520fdff503dce90c6bdc1710b26a9683a3dc2da
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-30 00:43:33 +0330

    Edit a methode

---
+++ CatA.java
@@ -0,0 +1,12 @@
+public class CatA  extends AnimalManagement{
+
+    public CatA(String name , int age){
+        super(name, age);
+    }
+    
+
+    public void makeSound(){
+       
+        System.out.println( "Cat Meows.");
+    }
+}
