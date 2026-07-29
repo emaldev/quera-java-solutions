f22cb3fc1a7f0ccf15e5b88fe7308147dcdfc21e
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-30 00:29:59 +0330

    the animal class is add methods

--- AnimalManagement.java
+++ AnimalManagement.java
@@ -1,3 +1,20 @@
 public class AnimalManagement {
-    
+    private String name;
+    private int age;
+
+    public AnimalManagement(Strin name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void makeSound(){
+        System.out.println("Animal makes a suond.");
+    }
 }
