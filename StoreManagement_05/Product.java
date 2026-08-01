7e9e71ed887520258f3badf499adaa2e7cef6e63
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-02 00:45:22 +0330

    Adding the getter methode

--- StoreManagement_05/Product.java
+++ StoreManagement_05/Product.java
@@ -1,14 +1,24 @@
 package StoreManagement_05;
 
 public class Product {
-    String name;
-    double price;
-    int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public  double getPrice(){
+        return price;
+    }
+    public int getQuantity(){
+        return quantity;
     }
     
 
