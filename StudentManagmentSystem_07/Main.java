fa41cafe2a6b32475452e5974506427893b69644
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-03 23:29:01 +0330

    addite a varaible

--- StudentManagmentSystem_07/Main.java
+++ StudentManagmentSystem_07/Main.java
@@ -3,6 +3,9 @@ package StudentManagmentSystem_07;
 public class Main {
     public static void main(String[] args){
         
+        School school = new School();
+        school.addStudents("Ali", 21, 3456);
+        school.foundStudents("Ali");
     }
     
 }
--- StudentManagmentSystem_07/School.java
+++ StudentManagmentSystem_07/School.java
@@ -7,22 +7,22 @@ public class School{
     ArrayList<Student> students = new ArrayList<>();
 
     // this method is for add a new student in the school.
-    public void addStudents(String name, int age, int is){
-        students.add(new Student(name, age, is));
+    public void addStudent(String name, int age, int id){
+        students.add(new Student(name, age, id));
     }
 
-    public Student foundStudents(String name){
+    public Student findStudent(String name){
         for(Student s: students){
             if(s.getName().equalsIgnoreCase(name)){
-                System.out.println("Fint students: " + s.getName());
+                System.out.println("find students: " + s.getName());
                 return s;
-            }else{
-                System.out.println("Student not found the school.");
             }
-            
+         
         }
+        System.out.println("Student not find the school.");
         return null;
+        
     }
 
-    
+
 }
