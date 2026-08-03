3ec50c2ce07f14bcb526655c100623065aa4c9e4
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-03 23:20:46 +0330

    Adding the add and found student

--- StudentManagmentSystem_07/School.java
+++ StudentManagmentSystem_07/School.java
@@ -2,8 +2,27 @@ package StudentManagmentSystem_07;
 
 import java.util.ArrayList;
 
-public class School {
-    ArrayList<Student> students = new ArrayList<>();
-    
+public class School{
 
+    ArrayList<Student> students = new ArrayList<>();
+
+    // this method is for add a new student in the school.
+    public void addStudents(String name, int age, int is){
+        students.add(new Student(name, age, is));
+    }
+
+    public Student foundStudents(String name){
+        for(Student s: students){
+            if(s.getName().equalsIgnoreCase(name)){
+                System.out.println("Fint students: " + s.getName());
+                return s;
+            }else{
+                System.out.println("Student not found the school.");
+            }
+            
+        }
+        return null;
+    }
+
+    
 }
