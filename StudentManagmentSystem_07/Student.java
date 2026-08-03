bdc3cc68f23a199c0c2abe5f54d951fb23292229
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-03 22:59:16 +0330

    this is my student information

--- StudentManagmentSystem_07/Student.java
+++ StudentManagmentSystem_07/Student.java
@@ -1,6 +1,26 @@
 package StudentManagmentSystem_07;
 
 public class Student {
-    
+    private String name;
+    private int age;
+    private int id;
+
+    // Creat a constractor.
+    public Student(String name , int age, int id){
+        this.name = name; 
+        this.age = age;
+        this.id = id;
+    }
+
+    // Creat getter for return methods.
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public int getId(){
+        return id;
+    }
     
 }
