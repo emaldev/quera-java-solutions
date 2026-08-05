14740f42c890abf1aafa25177eb17d456cd99872
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-06 00:13:50 +0330

    This is a banking system built through practice and design; to design the classes myself, I first sketch out my roadmap on paper and then begin coding.

--- BankManagementSystem_07/Main.java
+++ BankManagementSystem_07/Main.java
@@ -1,3 +1,7 @@
+//This is a banking system built through 
+// practice and design;
+//  to design the classes myself, I first
+//  sketch out my roadmap on paper and then begin coding.
 package BankManagementSystem_07;
 
 public class Main {
@@ -5,7 +9,12 @@ public class Main {
 
         Bank account = new Bank();
         account.addAccount("Milad", "456789002", 90);
+        account.addAccount("Asef Habibi", "23456789", 899);
         account.searchAccount("milad");
+        account.depositMoney("Milad", 100);
+        account.withdrawMoney("Asef Habibi", 100);
+
+        account.display();
         
     }
 }
