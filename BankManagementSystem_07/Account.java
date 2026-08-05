995bf80fe049a07c55616cffaf7ac77606ba3ab8
emaldev <261611436+emaldev@users.noreply.github.com>
2026-08-05 23:45:36 +0330

    this is iventory method

---
+++ BankManagementSystem_07/Account.java
@@ -0,0 +1,43 @@
+package BankManagementSystem_07;
+
+public class Account {
+    private String name;
+    private String acountNumber;
+    private float balance;
+
+    // make a constractor here 
+    public Account(String name, String acountNumber, float invertory ){
+        this.name = name;
+        this.acountNumber = acountNumber;
+        this.balance = invertory;
+    }
+
+
+    // display acount 
+    public void displayInfo(){
+        System.out.println("Name :" + name + ", Acount Number :" + acountNumber + ", Inventory : " + balance );
+
+    }
+
+    // this is the inventory method .
+    public void deposit(float amount){
+        balance += amount;
+        System.out.println("The payment has been made." + "{"+  balance + "}");
+    }
+
+    public void withdrawal(float  amount){
+
+        if(balance >= amount){
+            balance -= amount;
+            System.out.println("Withdrawal successful .");
+            System.out.println("Withdrawn: " + amount);
+            System.out.println("Balance : " + balance);
+        }else{
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+
+
+    
+}
